3390f38c22ba557b0c0bd5121ed45044570bb932
vishnupriya <vishnupriyayadav10@gmail.com>
2022-10-13 10:12:41 +0530

    commit

---
+++ Oct3/business/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+
+    public class books
+    {
+
+
+        public int bookid { get; set; }
+        public string bookname { get; set; }
+        public string bookauthor { get; set; }
+    }
+}
---
+++ Oct3/data/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data
+{
+    public class Class1
+    {
+        public List<books> GetProducts()
+        {
+            SqlConnection c = new SqlConnection("Data Source=DESKTOP-LUAVTH3;Initial Catalog=kannan;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select *from bookdetail", c);
+            c.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            List<books> plist = new List<books>();
+
+            while (dr.Read())
+            {
+                plist.Add(new books { bookid = Convert.ToInt32(dr[0]), bookname = dr[1].ToString(), bookauthor = dr[2].ToString() });
+
+            }
+            c.Close();
+            c.Dispose();
+            return plist;
+
+        }
+    }
+}
